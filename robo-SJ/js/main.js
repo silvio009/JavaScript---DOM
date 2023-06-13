df94ccc9c7c6fc1dd2c8ee224762b743aad0c92c
silvio009 <rm550821@fiap.com.br>
2023-06-13 16:03:24 -0300

    melhorando o codigo

--- robo-SJ/js/main.js
+++ robo-SJ/js/main.js
@@ -11,22 +11,18 @@ const subtrair = document.querySelector("#subtrair")
 const somar = document.querySelector("#somar");
 const braco = document.querySelector("#braco"); 
 
-const controle = document.querySelectoraAll(".controle-ajuste");
+const controle = document.querySelectorAll(".controle-ajuste");
 
-somar.addEventListener("click",function(evento){
-    manipulaDados("somar")
-})
-subtrair.addEventListener("click", function(evento){
-    manipulaDados("subtrair")
-})
+controle.forEach((elemento) => {
+    elemento.addEventListener("click", function(evento){
+     manipulaDados(evento.target.textContent)
+    })
+});
 
 function manipulaDados(operacao){
-        if(operacao === "subtrair"){
+        if(operacao === "-"){
             braco.value = parseInt(braco.value) -1
         }else{
             braco.value = parseInt(braco.value) +1
-        }
-
-    
-    
+        }   
 }
