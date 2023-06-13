eed62cd02910eb66280fb13f91c03db6495de5b3
silvio009 <rm550821@fiap.com.br>
2023-06-13 15:34:11 -0300

    add value

--- robo-SJ/js/main.js
+++ robo-SJ/js/main.js
@@ -6,3 +6,14 @@ function apresentacao(nome){
     console.log("olá", nome );
 }
 apresentacao("visitante")
+
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar");
+const braco = document.querySelector("#braco"); 
+
+somar.addEventListener("click",function(evento){
+  braco.value = parseInt(braco.value) +1
+})
+subtrair.addEventListener("click", function(evento){
+  braco.value = parseInt(braco.value) -1
+})
