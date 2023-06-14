7b6ff99b45efa8feab6f2ac61715d39ceb14e553
silvio009 <rm550821@fiap.com.br>
2023-06-14 15:35:00 -0300

    peças dinamicas

--- robo-SJ/js/main.js
+++ robo-SJ/js/main.js
@@ -7,15 +7,51 @@ function apresentacao(nome){
 }
 apresentacao("visitante")
 
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
 
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
 
 
 const controle = document.querySelectorAll("[data-controle]");
+const estatisticas = document.querySelectorAll("[data-estatistica]")
+
+
+
 
 controle.forEach((elemento) => {
     elemento.addEventListener("click", function(evento){
      manipulaDados(evento.target.dataset.controle , evento.target.parentNode)
-     
+     atualizaEstatiticas(evento.target.dataset.peca)
     })
 });
 
@@ -27,3 +63,9 @@ function manipulaDados(operacao, controle){
             peca.value = parseInt(peca.value) +1
         }   
 }
+ function atualizaEstatiticas(peca){
+
+   estatisticas.forEach((elemento)=>{
+         elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+   })
+ }
